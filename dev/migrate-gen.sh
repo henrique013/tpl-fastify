d51196efbb71ff5f764533b708cacc959e069687
henrique-mw <henrique@mundowap.com.br>
2025-05-28 11:05:07 -0300

    refactor(migrate): update docker command in migrate-gen.sh to use 'docker compose run' syntax

--- dev/migrate-gen.sh
+++ dev/migrate-gen.sh
@@ -9,4 +9,4 @@ if ! [[ $migration_name =~ ^[a-z0-9]+(-[a-z0-9]+)*$ ]]; then
 fi
 
 # generate the migration
-docker compose exec api ash -c "npx drizzle-kit generate --name $migration_name"
+docker compose run --rm api ash -c "npx drizzle-kit generate --name $migration_name"
