47f5515ff31d0c29949bdc8c5c70bcc1a6091a54
Henrique Alves <henrique013.a@gmail.com>
2025-05-30 10:26:26 -0300

    feat(di): add repositories registration to dependency injection container

--- src/di/container.ts
+++ src/di/container.ts
@@ -1,6 +1,8 @@
 import { container } from 'tsyringe'
 import { registerLibs } from '@di/providers/libs.js'
+import { registerRepos } from '@di/providers/repos.js'
 
 registerLibs(container)
+registerRepos(container)
 
 export { container }
