aeb2c909e6d7e9008dfce77778fddeaee6f909ab
Henrique Alves <henrique013.a@gmail.com>
2025-05-31 15:22:03 -0300

    feat(migrations): update drizzle configuration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'drizzle-kit'
 import { env } from './src/infra/env'
 
 export default defineConfig({
-  out: './drizzle',
-  schema: './src/db/schema.ts',
+  out: './src/infra/drizzle/migrations',
+  schema: './src/infra/drizzle/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
     url: env.PG_MIGRATIONS_URL,
