1d35f9b85be513d16d0bd1b887e6c2e47f0b5a1d
Henrique Alves <henrique013.a@gmail.com>
2025-06-03 16:47:58 -0300

    feat(postinstall): add post-install script to install lefthook in development environment

---
+++ bin/scripts/postinstall.ts
@@ -0,0 +1,15 @@
+import { execSync } from 'node:child_process'
+import { exit } from 'node:process'
+
+// check if we're in development environment
+const isDevelopment = process.env['NODE_ENV'] === 'development'
+
+if (isDevelopment) {
+  try {
+    console.log('Installing lefthook...')
+    execSync('lefthook install', { stdio: 'inherit' })
+  } catch (error) {
+    console.error('Error installing lefthook:', error)
+    exit(1)
+  }
+}
