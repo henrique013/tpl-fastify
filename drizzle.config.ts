4acc4577fab495344708599a7cde38f4d11182ed
Henrique Alves <henrique013.a@gmail.com>
2025-06-07 20:34:00 -0300

    refactor(config): update import paths and migrate command in configuration files

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'drizzle-kit'
-import { env } from './src/infra/env'
+import { env } from '@infra/env.js'
 
 export default defineConfig({
   out: './src/infra/orm/migrations',
