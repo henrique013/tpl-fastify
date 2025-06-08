a44edae059c057ef4251b6ee3f114324535f800b
Henrique Alves <henrique013.a@gmail.com>
2025-06-07 21:10:50 -0300

    fix(postinstall): update environment variable access method in postinstall script

--- src/infra/scripts/postinstall.ts
+++ src/infra/scripts/postinstall.ts
@@ -1,9 +1,8 @@
 import { execSync } from 'node:child_process'
 import { exit } from 'node:process'
-import { env } from '@infra/env.js'
 
 // check if we're in development environment
-const isDevelopment = env.NODE_ENV !== 'production'
+const isDevelopment = process.env['NODE_ENV'] !== 'production'
 
 if (isDevelopment) {
   try {
