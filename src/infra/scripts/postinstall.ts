4dbf20ebf1564ab34d10eb346f2c8dbc912b285b
Henrique Alves <henrique013.a@gmail.com>
2025-06-07 21:21:57 -0300

    fix(postinstall): update script to skip lefthook installation in production environment

--- src/infra/scripts/postinstall.ts
+++ src/infra/scripts/postinstall.ts
@@ -1,15 +1,18 @@
 import { execSync } from 'node:child_process'
 import { exit } from 'node:process'
 
-// check if we're in development environment
-const isDevelopment = process.env['NODE_ENV'] !== 'production'
+// check if we're in production environment
+const isProduction = process.env['NODE_ENV'] === 'production'
 
-if (isDevelopment) {
-  try {
-    console.log('Installing lefthook...')
-    execSync('lefthook install', { stdio: 'inherit' })
-  } catch (error) {
-    console.error('Error installing lefthook:', error)
-    exit(1)
-  }
+if (isProduction) {
+  console.log('Skipping lefthook installation in production environment')
+  exit(0)
+}
+
+try {
+  console.log('Installing lefthook...')
+  execSync('lefthook install', { stdio: 'inherit' })
+} catch (error) {
+  console.error('Error installing lefthook:', error)
+  exit(1)
 }
